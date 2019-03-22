a8999463908a4315d2e55b05babad7be0ceea02a
Lauren <laurenribeiro06@gmail.com>
2019-03-22 19:02:09 +0000

    Implement abilities for admin to create new categories and to see a list of all categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < ApplicationController
-
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  def index
   end
 
+  def new
+  end
+
+  def create
+  end
 end
